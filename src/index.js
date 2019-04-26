d22813f701cbce19cdf1165b35328071ec27d8a3
Brandon <brandon@reddirtwebdesign.com>
2019-04-26 00:54:28 -0700

    Working SideNav

--- src/index.js
+++ src/index.js
@@ -3,8 +3,17 @@ const path = require('path');
 const remote = electron.remote;
 const BrowserWindow = electron.remote.BrowserWindow;
 
+// Grab The Main New Order Button
 const newOrderButton = document.querySelector('#new-order-button');
 
+// Initialize The Materialize SideNav
+document.addEventListener('DOMContentLoaded', function() {
+	const elems = document.querySelectorAll('.sidenav');
+	const options = {};
+	const instances = M.Sidenav.init(elems, options);
+});
+
+// Creates The Add New Order Window
 newOrderButton.addEventListener('click', () => {
 	const formPath = path.join('file://', __dirname, 'new-order.html');
 	let win = new BrowserWindow({
